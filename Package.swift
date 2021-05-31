e603cfee3faba8ef470038f54189e7ff9d57a6aa
Go Takagi <15936908+shimastripe@users.noreply.github.com>
2021-05-31 11:02:41 +0900

    Update

--- Package.swift
+++ Package.swift
@@ -4,14 +4,14 @@
 import PackageDescription
 
 let package = Package(
-    name: "RxAsDataSources",
+    name: "RxASDataSources",
     platforms: [
         .iOS(.v10)
     ],
     products: [
         // Products define the executables and libraries produced by a package, and make them visible to other packages.
         .library(
-            name: "RxAsDataSources",
+            name: "RxASDataSources",
             targets: ["DataSources"]),
     ],
     dependencies: [
